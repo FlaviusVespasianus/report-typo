56fc1ca297fc3fdeda7f2164351ccd6e376ead3e
FlaviusVespasianus <antoninusheliogabalus@gmail.com>
2017-01-23 16:21:36 +0300

    Update typoMailer.php

--- m-module/typoMailer.php
+++ m-module/typoMailer.php
@@ -1,18 +1,20 @@
 <?php
-if (!($_POST["mbot"])) {
-    $url = $_POST["m-url"];
-    $mis = $_POST["m-mis"];
-    $comment = $_POST["m-comment"];
+if (!($_POST["m-bot"])) {
+    if (($_POST['mcomment']) || ($_POST['mmis'])) {
+        $url = $_POST["m-url"];
+        $mis = $_POST["m-mis"];
+        $comment = $_POST["m-comment"];
 
-    $to = "your@mail.com";
-    $subject = "Error on the webpage";
+        $to = "your@mail.com";
+        $subject = "Error on the webpage";
 
-    $email_content = "Adress: $url\n\n";
-    $email_content .= "Error: $mis\n\n";
-    $email_content .= "Message:\n$comment\n";
+        $email_content = "Adress: $url\n\n";
+        $email_content .= "Error: $mis\n\n";
+        $email_content .= "Message:\n$comment\n";
 
-    $sent = mail($to, $subject, $email_content);
-
-    print json_encode($sent);
-
+        $sent = mail($to, $subject, $email_content);
+        print json_encode($sent);
+    } else {
+        print 'error1';
+    }
 }
